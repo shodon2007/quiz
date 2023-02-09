db15630063ff2f9a89f9980ce1aa63e54b337e94
Speed431 <73215393+Speed431@users.noreply.github.com>
2023-02-09 18:42:01 +0700

    added basic questions

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [questions, setQuestion] = useState([
+    {
+      text: 'Что значить \'use-strict\'',
+      options: [
+        ['Новосибирск', false],
+        ['Москва', true],
+        ['Санкт-питербург', false],
+        ['Вашингтон', false],
+      ],
+      select: 0,
+    },
+    {
+      text: 'Привет',
+      options: [
+        ['Как', false],
+        ['Твои', true],
+        ['Прекрасные', false],
+        ['Дела', false],
+      ],
+      select: 0,
+    }
+  ]);
+  function optionsClick() {
+    if (thisQuestion < questions.length - 1) {
+      setThisQuiestion(thisQuestion + 1);
+    } else {
+      console.log('end')
+    }
+  }
+  const [thisQuestion, setThisQuiestion] = useState(0);
+  return (
+    <>
+      <div className='quiz__text'>
+        {questions[thisQuestion].text}
+      </div>
+      <div className="quiz__options">
+        <div onClick={() => optionsClick(0)}>{questions[thisQuestion].options[0]}</div>
+        <div onClick={() => optionsClick(1)}>{questions[thisQuestion].options[1]}</div>
+        <div onClick={() => optionsClick(2)}>{questions[thisQuestion].options[2]}</div>
+        <div onClick={() => optionsClick(3)}>{questions[thisQuestion].options[3]}</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
