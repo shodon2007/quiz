86969e2fe2da287aebbf787ffce3984e1762a88a
Speed431 <73215393+Speed431@users.noreply.github.com>
2023-02-12 10:58:01 +0700

    added responsive and fixed bugs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+let vh = window.innerHeight * 0.01;
+document.querySelector('body').style.setProperty('--vh', `${vh}px`);
+
+
 root.render(
   <React.StrictMode>
     <App />
