36e787a3a7252535de5a0d34db28472e4df99815
Speed431 <73215393+Speed431@users.noreply.github.com>
2023-02-12 20:18:10 +0700

    updated mobile version

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let vh = window.innerHeight * 0.01;
 document.querySelector('body').style.setProperty('--vh', `${vh}px`);
 
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 root.render(
   <React.StrictMode>
